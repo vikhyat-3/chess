3b3afb388086e3dc42b596e6800ce9ac6295c0d3
vikhyat-3 <82944741+vikhyat-3@users.noreply.github.com>
2021-05-24 10:52:58 +0530

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,3 +1,4 @@
 console.log("HELLO!");
 console.log("Welcome to our chess page :)");
 console.log("Please do note that the page is also responsive!\n");
+console.log("This is a simple chess page which tells us about the history of the game, explains the rules of chess to a beginner and shows the current top 10 chess players of the world.\n");
